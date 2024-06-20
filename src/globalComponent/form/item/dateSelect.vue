6508fe3d5432319a40aa20bc75f10776ceaef653
jiakaiqiang <13165792976@163.com>
2024-06-20 15:22:26 +0800

    交 息

--- src/globalComponent/form/item/dateSelect.vue
+++ src/globalComponent/form/item/dateSelect.vue
@@ -1,35 +1,28 @@
 <template>
-<div class='form-date'>
-   
-  <el-date-picker
-        v-model="dateValue"
-        type="date"
-        placeholder="请选择"
-       
-      @change="handleEvent"
-      />
-      
-      
-</div>
+  <div class='form-date'>
 
+    <el-date-picker v-model="dateValue" type="date" placeholder="请选择" @change="handleEvent" />
+
+
+  </div>
 </template>
 <script lang="ts" setup>
-  
 
-import { ref,defineProps,defineEmits,watch,defineModel } from 'vue';
+
+import { defineEmits, defineModel } from 'vue';
 
 
 const emit = defineEmits(['handleEvent'])
-let dateValue= defineModel()
+let dateValue = defineModel()
 
-const handleEvent = (val:any)=>{
-  emit('handleEvent','changeDate',val)
+const handleEvent = (val: any) => {
+  emit('handleEvent', 'changeDate', val)
 }
 
 </script>
 
 <style>
-.form-date{
+.form-date {
   width: 130px;
 }
 </style>
